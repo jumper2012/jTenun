b794db1ea6885e4305638ff1a846774b31f0f14c
Surya07 <simangunsong.seven@gmail.com>
2016-07-20 14:33:59 +0700

    itemobject recycleadapter, recyclehoder, and mainactivity

---
+++ app/src/main/java/bhouse/jtenun/RecyclerViewHolders.java
@@ -0,0 +1,28 @@
+package bhouse.jtenun;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by Sursev on 7/19/2016.
+ */
+public class RecyclerViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public TextView countryName;
+    public ImageView countryPhoto;
+
+    public RecyclerViewHolders(View itemView) {
+        super(itemView);
+        itemView.setOnClickListener(this);
+        countryName = (TextView)itemView.findViewById(R.id.country_name);
+        countryPhoto = (ImageView)itemView.findViewById(R.id.country_photo);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(view.getContext(), "Clicked Country Position = " + getPosition(), Toast.LENGTH_SHORT).show();
+    }
+}
